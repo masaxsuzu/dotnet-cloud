41a5cf8c1fed8347d189666a0fa6571cdb28cc5e
masaxsuzu <masaxsuzu+github@gmail.com>
2020-07-07 09:30:42 +0900

    Add src

---
+++ src/Netsoft.Clound.App1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Netsoft.Clound.App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Amazon ECS!");
+        }
+    }
+}
